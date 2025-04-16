86154c0b6a55d5d49629083cc07813582ead4f58
metelskiwiktor <metelski.wiktor@gmail.com>
2025-04-16 15:16:03 +0200

    init commit

---
+++ src/main/java/pl/wiktor/devaudit/DevauditBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.wiktor.devaudit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevauditBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevauditBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/wiktor/devaudit/DevauditBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wiktor.devaudit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevauditBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
